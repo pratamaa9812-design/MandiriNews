c07abf2bfa390f56593fa8651a03186e5cf43328
Rifki Pratama <pratamaa9812@gmail.com>
2025-11-18 13:26:09 +0800

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-}
+
 
 android {
     namespace = "dev.rakamin.newsapp"
